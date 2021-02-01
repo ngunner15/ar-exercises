646653badb56e58e38f150e0542906d9e41c67b8
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-01 16:32:34 -0500

    Refractor and custom validation

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,12 @@
 class Store < ActiveRecord::Base
   has_many :employees
+  validates :name, length: { minimum: 3 }
+  validates :annual_revenue, numericality: { greater_than_or_equal_to: 0 }
+  validate :apparel_gender
+  def apparel_gender
+      if !mens_apparel && !womens_apparel
+        errors.add(:mens_apparel, "must be true for least one apparel type")
+        errors.add(:womens_apparel, "must be true for least one apparel type")
+      end
+  end
 end
