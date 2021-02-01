4e59e93be4129cea8b4c0fbe996515210aec7f61
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-01 15:57:29 -0500

    Create one to many relationship between store and employee

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,9 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Nisarg", last_name: "Vadgama", hourly_rate: 40)
+@store1.employees.create(first_name: "Aaron", last_name: "Ramsey", hourly_rate: 50)
+
+@store2.employees.create(first_name: "Jose", last_name: "Mourinho", hourly_rate: 68)
+@store2.employees.create(first_name: "Harry", last_name: "Kane", hourly_rate: 58)
