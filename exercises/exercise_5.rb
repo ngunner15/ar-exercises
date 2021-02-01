1edac3bb4f865ce3ab827cb3c501d7c577107977
Nisarg Vadgama <nisargvv@gmail.com>
2021-02-01 15:46:12 -0500

    Greater than and Count queries

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,9 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+total = Store.sum('annual_revenue')
+puts total
+puts total/Store.count
+
+@greater_than_1m = Store.where(Store.arel_table[:annual_revenue].gteq(1000000)).count
+puts @greater_than_1m
